85ad12bec89c68250f495f480654c427aa42b802
jreynaud2 <jean.reynaud@ecal.ch>
2020-04-12 20:18:35 +0200

    formate

--- site/templates/tracks.json.php
+++ site/templates/tracks.json.php
@@ -9,15 +9,15 @@ foreach($data as $track) {
 
   $json[] = [
     // if user as select: genre +
-    'artist'   => (string)$track->artist(),
+    'artist'     => (string)$track->artist(),
     'tracktitle' => (string)$track->tracktitle(),
-    'date'  => (string)$track->date(),
-    'label'  => (string)$track->label(),
-    'genre'  => (string)$track->genre(),
-    'duration'  => (string)$track->duration(),
-    'text'  => (string)$track->text(),
-    'cover'  => (string)$track->cover(),
-    'mp3'  => (string)$track->mp3()
+    'date'       => (string)$track->date(),
+    'label'      => (string)$track->label(),
+    'genre'      => (string)$track->genre(),
+    'duration'   => (string)$track->duration(),
+    'text'       => (string)$track->text(),
+    'cover'      => (string)$track->cover(),
+    'mp3'        => (string)$track->mp3()
 
   ];
 
