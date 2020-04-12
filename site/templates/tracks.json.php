7b1184d7a117d89d5a523931b36004d056ed47d0
jreynaud2 <jean.reynaud@ecal.ch>
2020-04-12 17:31:23 +0200

    jsonfrontmodify

--- site/templates/tracks.json.php
+++ site/templates/tracks.json.php
@@ -3,14 +3,17 @@
 $data = $pages->find('tracks')->children()->published()->flip();
 $json = [];
 
+// condition get user choice from frontend --> modify data
+
 foreach($data as $track) {
 
   $json[] = [
+    // if user as select: genre +
     'url'   => (string)$track->url(),
     'title' => (string)$track->title(),
     'text'  => (string)$track->text(),
     'date'  => (string)$track->date()
-  ];
+  ]; 
 
 }
 
