6765f1b7bf9f3c6d434fe2ea3c51ae5aa31c573a
sam69t <samueldumezz@yahoo.fr>
2020-04-12 23:15:38 +0200

    exemple ampli.js

---
+++ assets/js/playlist.js
@@ -0,0 +1,18 @@
+Amplitude.init({
+"songs": [
+{
+"artist": "Song 1",
+"tracktitle": "artist 1",
+"date": "Album 1",
+"label": "audio/song1.m4a",
+"genre": "image1.png",
+"duration": "image1.png",
+"text": "image1.png",
+"cover": "image1.png",
+"mp3": "mp3"
+
+
+
+
+}]
+ });
